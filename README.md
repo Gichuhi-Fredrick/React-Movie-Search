2bd162f0caffde5a42977db948889c78a7fe1caf
Fredrick <fredgkimani@gmail.com>
2022-03-27 11:23:35 +0300

    background and animation

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+### Clone this repo
+
+1 Run npm install
+2 Run npm start
+3 You must add your own API key in the src/App.js
+
+### GET API KEY
+
+1 Signin or Register at https://www.themoviedb.org/signup
+2 Under the Account icon, click Settings
+3 On the API page, click on the link under the Request an API Key section.
+4 Register an API key as a Developer
+5 Agree to the terms of use and fill in the required information:
+6 You will see an API key displayed under API Key (v3 auth). Copy the key and
+paste it to the APIKEY field in src/App.js.
+
 ### Links
 
 - GitHub URL: (https://github.com/Gichuhi-Fredrick/React-Movie-Search)
--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import Title from './Components/Title'
-import SearchMovies from './Components/SearchMovies'
-import TrendingMovies from './Components/TrendingMovies'
-import './styles.css'
+import React, { useState, useEffect } from 'react';
+import Title from './Components/Title';
+import SearchMovies from './Components/SearchMovies';
+import TrendingMovies from './Components/TrendingMovies';
+import './styles.css';
 
+// HOT TO GET API KEY
+// Signin or Register at https://www.themoviedb.org/signup
+// Under the Account icon, click Settings
+// On the API page, click on the link under the Request an API Key section.
+// Register an API key as a Developer
+// Agree to the terms of use and fill in the required information:
+// You will see an API key displayed under API Key (v3 auth). Copy the key and paste it to the APIKEY field below
+
+const APIKEY = '27dafd812c1e71b956120968116eaf3f';
 export default function App() {
-	const [trending, setTrending] = useState([])
+  const [trending, setTrending] = useState([]);
+  const url = `https://api.themoviedb.org/3/search/trending?api_key=${APIKEY}&language=en-US&media_type=tv&time_window=day`;
 
-	useEffect(() => {
-		const data = async () => {
-			const trendingUrl = `https://api.themoviedb.org/3/search/trending?api_key=27dafd812c1e71b956120968116eaf3f&language=en-US&media_type=tv&time_window=day`
+  useEffect(() => {
+    const data = async () => {
+      const trendingUrl = url;
 
-			let response = await fetch(trendingUrl)
-			let data = await response.json()
+      let response = await fetch(trendingUrl);
+      let data = await response.json();
 
-			setTrending(data.results)
-		}
-		data()
-	}, [setTrending])
+      setTrending(data.results);
+    };
+    data();
+  }, [setTrending, url]);
 
-	return (
-		<div className="container">
-			<Title />
-			<SearchMovies />
-			<TrendingMovies trending={trending} />
-		</div>
-	)
+  return (
+    <div className="container">
+      <Title />
+      <SearchMovies />
+      <TrendingMovies trending={trending} />
+    </div>
+  );
 }
